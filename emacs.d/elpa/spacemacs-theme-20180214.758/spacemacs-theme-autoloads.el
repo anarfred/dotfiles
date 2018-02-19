5df9fffe38eb838bcb56fa04ddb2e702bc3a62b3
Frédéric Vachon <frederic.vachon.1@ulaval.ca>
2018-02-19 14:58:52 -0500

    add emacs

---
+++ emacs.d/elpa/spacemacs-theme-20180214.758/spacemacs-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (23176
+;;;;;;  45564 23976 75000))
+;;; Generated autoloads from spacemacs-common.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
+;;;;;;  "spacemacs-theme-pkg.el") (23176 45564 20642 741000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; spacemacs-theme-autoloads.el ends here
