119485b14729d9ff1ef15834cc5299d7c10a586d
Frédéric Vachon <frederic.vachon.1@ulaval.ca>
2018-03-26 23:17:03 -0400

    Maj mars

--- emacs.d/config.el
+++ emacs.d/config.el
@@ -1,31 +1,142 @@
 
-(setq inhibit-startup-message t)
-
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq ring-bell-function 'ignore)
-
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-(when window-system (global-hl-line-mode t))
-
-(when window-system (global-prettify-symbols-mode t))
-
-(defvar my-term-shell "/bin/zsh")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
-
-(use-package org-bullets
+(use-package elfeed
   :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
+  ;; :bind (:map elfeed-search-mode-map
+  ;;             ("A" . bjm/elfeed-show-all)
+  ;;             ("E" . bjm/elfeed-show-emacs)
+  ;;             ("D" . bjm/elfeed-show-daily)
+  ;;             ("q" . bjm/elfeed-save-db-and-bury))
+)
+
+(defun ga/play-with-mpv (start end)
+"Play the link in the region with mpv"
+(interactive "r")
+(shell-command (concat "mpv " (buffer-substring start end) "\&")))
+
+(define-key elfeed-show-mode-map (kbd "C-c o") 'ga/play-with-mpv)
+
+(setq elfeed-feeds
+  '("http://blog.martinfenner.org/rss.xml"
+    "http://jonreeve.com/feed.xml"
+    "http://programminghistorian.org/feed.xml"
+    "http://saulalbert.net/feed/"
+    "http://www.docear.org/feed/"
+    "http://sonal.hypotheses.org/feed"
+    "http://literaturegeek.com/atom.xml"
+    "http://wcm1.web.rice.edu/feed.xml"
+    "http://spatialrecology.org/feed.xml"
+    "http://mph.puddingbowl.org/feed/"
+    "http://mydesiringmachines.wordpress.com/feed/"
+    "http://visualmethodculture.wordpress.com/feed/"
+    "http://progressivegeographies.com/feed/"
+    "http://fucklemonde.blogspot.com/feeds/posts/default"
+    "http://sexgeek.wordpress.com/feed/"
+    "https://replikradicale.wordpress.com/feed/"
+    "http://radicalbi.wordpress.com/feed/"
+    "http://jgieseking.org/feed/"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=jcsb0l&ai=15j&jc=rvst20&type=etoc&feed=rss"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=dxxykw&ai=wo&jc=gide20&type=etoc&feed=rss"
+    "http://redtac.org/possibles/feed/"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=lppxwi&ai=1c8&jc=wgls20&type=etoc&feed=rss"
+    "http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1541-0064"
+    "http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1548-744X"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=ej1ex6&ai=1a22g&jc=rprn20&type=etoc&feed=rss"
+    "http://www.jssj.org/feed/?post_type=jssj-article"
+    "http://rss.sciencedirect.com/publication/science/17554586"
+    "http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1540-6040"
+    "http://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=10508&channel-name=Archives+of+Sexual+Behavior"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=gugiih&ai=v4&jc=csos20&type=etoc&feed=rss"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=dxxykw&ai=2il&jc=rscg20&type=etoc&feed=rss"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=dxxykw&ai=1cy&jc=wjhm20&type=etoc&feed=rs"
+    "http://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=usja&type=etoc&feed=rss"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=eyhqfg&ai=1qz&jc=rmob20&type=etoc&feed=rss"
+    "http://journals.sagepub.com/action/showFeed?ui=0&mi=ehikzz&ai=2b4&jc=cgjb&type=etoc&feed=rss"
+    "http://glq.dukejournals.org/rss_feeds/current.xml"
+    "http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1467-8330"
+    "http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1475-4762"
+    "http://socialtext.dukejournals.org/rss_feeds/current.xml"
+    "http://www.tandfonline.com/action/showFeed?ui=0&mi=dxxykw&ai=2g1&jc=cgpc20&type=etoc&feed=rss"
+    "http://jeanneemard.wordpress.com/feed/"
+    "https://jacobinmag.com/feed/"
+    "http://willchernoff.com/feed/"
+    "http://anarchism.pageabode.com/rss.xml"
+    "http://beauvoirpapineau.blogspot.com/feeds/posts/default"
+    "http://libcom.org/rss.xml"
+    "http://www.anarchogeekreview.com/feed"
+    "http://psychologic-anarchist.com/feed/"
+    "http://nicolasphebus.tumblr.com/rss"
+    "http://www.lapointelibertaire.org/node/feed"
+    "http://flegmatique.net/feed/"
+    "http://moutonmarron.blogspot.com/feeds/posts/default"
+    "http://www.chronicle.com/blogs/profhacker/feed"
+    "https://kieranhealy.org/index.xml\"Furry\""
+    "http://furscience.com/feed/"
+    "http://www.adjectivespecies.com/feed/"
+    "http://lovesexfur.com/feed/"
+    "http://www.furwhatitsworth.com/?feed=podcast"
+    "https://www.flayrah.com/rss.xml"
+    "https://furrywritersguild.com/rss.xml"
+    "https://ricochet.media/fr/fil"
+    "http://feeds.feedburner.com/d0od"
+    "http://feeds2.feedburner.com/webupd8"
+    "https://www.archlinux.org/feeds/news/"
+    "https://www.neomutt.org/feed.xml"
+    "https://zedshaw.com/feed/"
+    "http://francoisestereo.com/feed/"
+    "https://voir.ca/mickael-bergeron/feed/"
+    "https://voir.ca/chanceuxqueteux/feed/"
+    "https://voir.ca/jean-felix-chenier/feed/"
+    "https://voir.ca/pierre-luc-senecal/feed/"
+    "https://voir.ca/cherchernoise/feed/"
+    "https://voir.ca/pierre-yves-mcsween/feed/"
+    "http://www.journaldemontreal.com/blogues/iris/rss.xml"
+    "http://www.journaldemontreal.com/blogues/leo-paul-lauzon/rss.xml"
+    "http://www.journaldemontreal.com/blogues/catherine-dorion/rss.xml"
+    "http://www.journaldemontreal.com/blogues/josee-legault/rss.xml"
+    "http://geekographie.maieul.net/spip.php?page=backend"
+    "http://lunduke.com/feed/"
+    "http://feeds.feedburner.com/FreedomPenguin"
+    "https://www.youtube.com/feeds/videos.xml?user=gotbletu"
+    "https://www.youtube.com/feeds/videos.xml?user=AwakenWithJP"
+    "https://www.youtube.com/feeds/videos.xml?user=ChrisWereDigital"
+    "https://www.youtube.com/feeds/videos.xml?user=BrianLunduke"
+    "https://www.youtube.com/feeds/videos.xml?user=JamesNintendoNerd"
+    "https://www.youtube.com/feeds/videos.xml?user=ICARUSLIV3S"
+    "https://www.youtube.com/feeds/videos.xml?user=phreakindee"
+    "https://www.youtube.com/feeds/videos.xml?user=SGtMarkIV"
+    "https://www.youtube.com/feeds/videos.xml?user=BadEditPro"
+    "https://www.youtube.com/feeds/videos.xml?user=JtheLinuxguy"
+    "https://www.youtube.com/feeds/videos.xml?user=connermcd"
+    "https://www.youtube.com/feeds/videos.xml?user=kaihendrai"
+    "https://www.youtube.com/feeds/videos.xml?user=criticalears2012"
+    "https://www.youtube.com/feeds/videos.xml?user=Kurzgesagt"
+    "https://www.youtube.com/feeds/videos.xml?user=DNewsChannel"
+    "https://www.youtube.com/feeds/videos.xml?user=scishow"
+    "https://www.youtube.com/feeds/videos.xml?user=scishowspace"
+    "https://www.youtube.com/feeds/videos.xml?user=sexplanations"
+    "https://www.youtube.com/feeds/videos.xml?user=BlackoutMusicNL"
+    "https://www.youtube.com/feeds/videos.xml?user=theneedledrop"
+    "https://www.youtube.com/feeds/videos.xml?user=PailinsKitchen"
+    "https://www.youtube.com/feeds/videos.xml?user=voxdotcom"
+    "https://www.youtube.com/feeds/videos.xml?user=FitOnRaw"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCbJrj3osfxL-U1ZtQ58EH5Q"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC3VWbWk8qDBiF0741izgpQg"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCXPHFM88IlFn68OmLwtPmZA"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCcQsDUZiK1GWDcP7BpVO_kw"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCCSHWqosFfYJY5v2WqbTLhg"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC7_gcs09iThXybpVgjHZ_7g"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCUdettijNYvLAm4AixZv4RA"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCn8V3KNSgDr1Dai77_y8JrQ"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCyFUj2oLZ1xkjK4sSUab-jA"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCi4uPLyx-LRhniBOLo-36bQ"
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCvA_wgsX6eFAOXI8Rbg_WiQ"
+    "https://viedelicorne.blog/feed/"
+    "https://xaviercamus.com/feed/"
+    "http://nullprogram.com/feed/"
+    "http://planet.emacsen.org/atom.xml"))
+
+(set-face-attribute 'variable-pitch (selected-frame) :font (font-spec :family "DejaVuSans" :size 16))
 
 (use-package helm
   :ensure t
@@ -82,6 +193,42 @@
   helm-mode-fuzzy-match t
   helm-completion-in-region-fuzzy-match t)
 )
+
+(setq inhibit-startup-message t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq ring-bell-function 'ignore)
+
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(when window-system (global-hl-line-mode t))
+
+(when window-system (global-prettify-symbols-mode t))
+
+(use-package spaceline
+  :ensure t
+  :config
+  (require 'spaceline-config)
+    (setq spaceline-buffer-encoding-abbrev-p nil)
+    (setq spaceline-line-column-p nil)
+    (setq spaceline-line-p nil)
+    (setq powerline-default-separator (quote arrow))
+    (spaceline-spacemacs-theme))
+
+(line-number-mode 1)
+(column-number-mode 1)
+
+(setq display-time-24hr-format t)
+(setq display-time-format "%H:%M - %d %B %Y")
+
+(display-time-mode 1)
 
 (setq scroll-conservatively 100)
 
@@ -150,20 +297,12 @@
   (message "MPD Database Updated!"))
 (global-set-key (kbd "s-m u") 'mpd/update-database)
 
-(use-package spaceline
+(use-package org-bullets
   :ensure t
   :config
-  (require 'spaceline-config)
-    (setq spaceline-buffer-encoding-abbrev-p nil)
-    (setq spaceline-line-column-p nil)
-    (setq spaceline-line-p nil)
-    (setq powerline-default-separator (quote arrow))
-    (spaceline-spacemacs-theme))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
 
-(line-number-mode 1)
-(column-number-mode 1)
-
-(setq display-time-24hr-format t)
-(setq display-time-format "%H:%M - %d %B %Y")
-
-(display-time-mode 1)
+(defvar my-term-shell "/bin/zsh")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,7 +20,7 @@
 (use-package base16-theme
   :ensure t
   :config
-  (load-theme 'base16-default-dark t))
+  (load-theme 'base16-google-dark t))
 
 ;;;;;
 
@@ -35,13 +35,15 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#32302F" "#FB4934" "#B8BB26" "#FABD2F" "#83A598" "#D3869B" "#17CCD5" "#EBDBB2"])
- '(custom-enabled-themes (quote (base16-default-dark)))
+ '(ansi-term-color-vector
+   [unspecified "#1f2022" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#4f97d7" "#a3a3a3"] t)
+ '(custom-enabled-themes (quote (base16-google-dark)))
  '(custom-safe-themes
    (quote
-    ("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "d9dab332207600e49400d798ed05f38372ec32132b3f7d2ba697e59088021555" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a4d03266add9a1c8f12b5309612cbbf96e1291773c7bc4fb685bfdaf83b721c6" default)))
+    ("aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "12670281275ea7c1b42d0a548a584e23b9c4e1d2dabb747fd5e2d692bcd0d39b" "36282815a2eaab9ba67d7653cf23b1a4e230e4907c7f110eebf3cdf1445d8370" "3de3f36a398d2c8a4796360bfce1fa515292e9f76b655bb9a377289a6a80a132" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "0c3b1358ea01895e56d1c0193f72559449462e5952bded28c81a8e09b53f103f" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "d9dab332207600e49400d798ed05f38372ec32132b3f7d2ba697e59088021555" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a4d03266add9a1c8f12b5309612cbbf96e1291773c7bc4fb685bfdaf83b721c6" default)))
  '(package-selected-packages
    (quote
-    (mingus spaceline base16-theme helm-swoop emms helm-descbinds helm org-bullets beacon spacemacs-theme darktooth-theme which-key use-package)))
+    (elfeed-org elfeed mingus spaceline base16-theme helm-swoop emms helm-descbinds helm org-bullets beacon spacemacs-theme darktooth-theme which-key use-package)))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8"))
 ;;(custom-set-faces
@@ -56,3 +58,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(set-face-attribute 'default nil :height 90)
